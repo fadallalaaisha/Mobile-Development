046aefd5af4698e0932207bc74d81df799cf37c4
fadallalaaisha <aishafadallala@gmail.com>
2021-02-28 10:07:35 +0300

    Second assignment

---
+++ src/assignment2.kt
@@ -0,0 +1,28 @@
+fun main() {
+    println("Hello Aisha")
+    division()
+    addition()
+    println(false)
+}
+fun aisha() {
+    println("Hello Aisha")
+}
+fun division() {
+    var y=27
+    var z=4
+    var divide=y/z
+    println(divide)
+}
+fun addition() {
+    var num1=2
+    var num2=4
+    var num3=6
+    var num4=1
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun me() {
+    var me= ("Aisha is very fat")
+    println(false)
+
+}
